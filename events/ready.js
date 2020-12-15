741c294428f4f290efd612979e1214c3e01fe4ed
Jean Staffe <69088819+IceroDev@users.noreply.github.com>
2020-12-15 16:49:12 +0100

    Ajout du bot

--- events/ready.js
+++ events/ready.js
@@ -1,3 +1,24 @@
 module.exports = (client) => {
-  console.log(`Je suis connecté et prêt à servir `+client.guilds.cache.size +" serveurs et "+client.users.cache.size+" utilisateurs");
+  const Discord = require("discord.js");
+  const axios = require("axios");
+  client.channels.cache.get("788420999871070228").messages.fetch("788425388187058196").then(m =>{
+    setInterval(async () => {
+      axios
+        .get('https://epistat.sciensano.be/Data/COVID19BE_CASES_MUNI_CUM.json')
+        .then(response => {
+          let cas = response.data[363].CASES
+        let embed = new Discord.MessageEmbed()
+        .setTitle("Cas de covid-19 à Tournai ")
+        .setDescription("C'est rien, c'est Tournai")
+        .addField("\u200b", "__**"+cas+"**__ imbéciles ont déjà chopé la covid-19. Alors mets ton masque bordel !")
+        
+        .setFooter("Dernière actualisation des données ")
+        .setTimestamp()
+        .setThumbnail("https://flespi.io/covid19/img/android-icon-192x192.a7ab640c.png")
+        .setImage("https://www.visitwapi.be/pivotimg/0/0/aHR0cDovL2lkZXRhLmJveGdtdC5uZXQvZmlsZXMvQU5YLTAyLTAzUzItMDAwMS9BTlgtMDItMDNTMi0wMDAxLmpwZw==.jpg")
+        .setColor("2f3136");
+            m.edit(embed)
+          })
+    }, 30000);
+  })
 }
